08b9f42bf7933d7f24a8a06b57ca7edbf25b0d14
Trey jackson <treyjackson@treys-mbp.attlocal.net>
2018-12-11 22:52:12 -0600

    updates

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/new/order', function(req, res, next) {
+router.post('/new/order', function(req, res, next) {
   console.log(req.body)
   res.send()
 });
