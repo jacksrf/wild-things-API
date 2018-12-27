e12d05cda0ca72070b03810a19db427f9115a000
Trey jackson <treyjackson@Treys-MacBook-Pro.local>
2018-12-27 13:55:52 -0600

    upload check

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+global.window = {document: {createElementNS: () => {return {}} }};
+global.navigator = {};
+global.btoa = () => {};
 var router = express.Router();
 var rest = require('restler');
 var fs = require('fs');
@@ -9,13 +12,17 @@ var apiKey = '4081512935093e026334dc4561b90ef6';
 var apiSecret = '7a060446ac0dda5075ea628e2595ee39';
 var redirectUri = 'http://localhost:8090/generate_token';
 var shopifyURIapi = 'https://' + apiKey + ':' + apiSecret + '@'+ shop;
+var jsPDF = require('jspdf');
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 router.get('/', function(req, res, next) {
   request('http://localhost:8090/order/pdf/5c14273221a30375248c4293', function (error, response, html) {
     if (!error && response.statusCode == 200) {
       console.log(html);
-      var options = { format: 'Letter' };
-
+      var options = { "height": "14in", "width": "8.5in" };
+      console.log(html);
       pdf.create(html, options).toFile('./public/pdf/businesscard.pdf', function(err, res) {
         if (err) return console.log(err);
         console.log(res); // { filename: '/app/businesscard.pdf' }
@@ -59,6 +66,22 @@ router.get('/order/pdf/:id', function(req, res, next) {
     console.log(order);
     res.render('order', {"order": order })
   } )
+})
+
+router.post('/order/pdf/save/:id', multipartMiddleware, function(req, res, next) {
+
+  var id = req.params.id;
+  var filename  = './'+ id +'.pdf';
+  console.log(id);
+  var db = req.db;
+  var file = req.body
+  console.log(file)
+  var pdf = new jsPDF('p', 'mm', 'a4');
+  pdf.addImage(file.image, 'PNG', 0, 0, 211, 298);
+  // console.log(pdf)
+  // var data = pdf.output();
+  //
+  // fs.writeFileSync(filename, data);
 })
 
 module.exports = router;
