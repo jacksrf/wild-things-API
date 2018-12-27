a872a97f029a074e2d4da144c8e0c41c83eb7aec
Trey jackson <treyjackson@Treys-MacBook-Pro.local>
2018-12-27 16:22:12 -0600

    updates

--- routes/index.js
+++ routes/index.js
@@ -43,11 +43,11 @@ router.get('/', function(req, res, next) {
         }
 
   webshot("http://localhost:8090/order/pdf/5c14273221a30375248c4293", "./public/pdf/test.png", options, function(){
-    var html_parsed = '<img src="./public/pdf/test.png"/>'
-    pdf.create(html_parsed, options2).toFile('./public/pdf/order.pdf', function(err, res) {
-      if (err) return console.log(err);
-      console.log(res); // { filename: '/app/businesscard.pdf' }
-    });
+    // var html_parsed = '<img src="./public/pdf/test.png"/>'
+    // pdf.create(html_parsed, options2).toFile('./public/pdf/order.pdf', function(err, res) {
+    //   if (err) return console.log(err);
+    //   console.log(res); // { filename: '/app/businesscard.pdf' }
+    // });
   });
 
 });
