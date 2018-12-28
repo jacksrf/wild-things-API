f256f9d1f886dd256e5397b4d14f91efc6e25072
Trey jackson <treyjackson@Treys-MacBook-Pro.local>
2018-12-27 20:45:16 -0600

    new app changes

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ router.get('/', function(req, res, next) {
             'height': 2200
         }
 
-  webshot("https://api.alsflowersmontgomery.com/order/pdf/5c14273221a30375248c4293", "./public/pdf/test.png", options, function(err) {
+  webshot("https://admin.alsflowersmontgomery.com/order/pdf/5c14273221a30375248c4293", "./public/pdf/test.png", options, function(err) {
 
 
     // var html_parsed = '<img src="./public/pdf/test.png"/>'
