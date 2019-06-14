bb9b40d71a1ac149b90e9d647eed4400b2826125
Trey jackson <treyjackson@MacBook-Pro.localdomain>
2019-06-14 13:15:48 -0500

    reset printer id

--- routes/index.js
+++ routes/index.js
@@ -147,7 +147,7 @@ router.get('/order/reprint/pdf/:id', function(req, res, next) {
                     // console.log(printer.printer_id)
               var formData = {
                     // "printer": printer.printer_id,
-                    "printer": 551547,
+                    "printer": 545151,
                     "title": "Order: "+ doc.order_number,
                     "contentType": "pdf_uri",
                     "content": "https://api.alsflowersmontgomery.com/pdf/"+ doc._id +".pdf",
