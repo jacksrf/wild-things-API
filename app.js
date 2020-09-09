f5965a70603a2c2db11e72b1951687ed288bd9e1
Rolan jackson <rolanjackson@Treys-MBP.localdomain>
2020-09-09 13:37:36 -0500

    updates to env file

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var mongo = require('mongodb');
 var monk = require('monk');
-
+console.log(process.env.DB_USERNAME)
+console.log(process.env.DB_KEY)
 // var db = monk('mongodb://jacksrf2:trey3333@ds155461.mlab.com:55461/als-flowers-api');
 var db = monk('mongodb+srv://'+ process.env.DB_USERNAME +':'+ process.env.DB_KEY +'@wild-things.vffvg.mongodb.net/wild-things?retryWrites=true&w=majority');
 console.log(db.collection)
